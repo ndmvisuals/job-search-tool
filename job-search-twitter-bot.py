69d79c9a7c2f6670113b427d968d626a8631a62d
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2022-04-25 14:58:43 -0400

    Update job-search-twitter-bot.py

--- job-search-twitter-bot.py
+++ job-search-twitter-bot.py
@@ -159,24 +159,24 @@ client = tweepy.Client(consumer_key= os.environ.get('CONSUMER_KEY'),
 
 # In[5]:
 
-client.create_tweet(text= "test from github")
-
-#today = date.today()
-#yesterday = today - timedelta(days = 1)
 
 
-#schedule = [[ "@washingtonpost", df_wapo, yesterday, "Yesterday"], 
-#["@npr", df_npr, yesterday, "Yesterday" ], 
-#["@nyt", df_nyt, yesterday, "Yesterday"]  ]
+today = date.today()
+yesterday = today - timedelta(days = 1)
 
-#for post in schedule:
-#    main_tweet, thread_posts, tweet_status = construct_tweet(post[0], post[1], post[2], post[3])
 
-#    print(main_tweet)
-#    print(tweet_status)
-#    for i in thread_posts:
-#        print(i)
+schedule = [[ "@washingtonpost", df_wapo, yesterday, "Yesterday"], 
+["@npr", df_npr, yesterday, "Yesterday" ], 
+["@nyt", df_nyt, yesterday, "Yesterday"]  ]
 
-#    if tweet_status == "y":
-#        publish_tweet(main_tweet, thread_posts, client)   
+for post in schedule:
+    main_tweet, thread_posts, tweet_status = construct_tweet(post[0], post[1], post[2], post[3])
+
+    print(main_tweet)
+    print(tweet_status)
+    for i in thread_posts:
+        print(i)
+
+    if tweet_status == "y":
+        publish_tweet(main_tweet, thread_posts, client)   
 
